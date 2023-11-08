3df373198dc21c9813561594e972b9c85655cbde
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2023-11-08 21:55:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/esjumbo/data/SumberData.kt
@@ -0,0 +1,13 @@
+package com.example.esjumbo.data
+
+import com.example.esjumbo.R
+
+object SumberData {
+    val flavor = listOf(
+        R.string.natural,
+        R.string.vanila,
+        R.string.lemon,
+        R.string.milk,
+        R.string.lyche
+    )
+}
