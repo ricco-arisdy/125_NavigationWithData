054dad098b288b72109cea893b5d0abacb183639
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2023-11-16 15:48:00 +0700

    create fun data pelanggan

--- app/src/main/java/com/example/esjumbo/HalamanSatu.kt
+++ app/src/main/java/com/example/esjumbo/HalamanSatu.kt
@@ -1,2 +1,111 @@
 package com.example.esjumbo
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HalamanSatu(
+    onSubmitButtonClick: (MutableList<String>) -> Unit,
+    onCancelButtonClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+    var namaTxt by rememberSaveable {
+        mutableStateOf("")
+    }
+    var alamatTxt by rememberSaveable {
+        mutableStateOf("")
+    }
+    var telponTxt by rememberSaveable {
+        mutableStateOf("")
+    }
+
+    var listDataTxt: MutableList<String> = mutableListOf(namaTxt, alamatTxt, telponTxt)
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize()
+    ) {
+        Text(
+            text = "Data Pelanggan",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(16.dp))
+        OutlinedTextField(value = namaTxt, onValueChange = {
+            namaTxt = it
+        }, label = {
+            Text(text = "Nama")
+        })
+        Spacer(modifier = Modifier.padding(16.dp))
+        OutlinedTextField(value = alamatTxt, onValueChange = {
+            alamatTxt = it
+        }, label = {
+            Text(text = "Alamat")
+        })
+        Spacer(modifier = Modifier.padding(16.dp))
+        OutlinedTextField(value = telponTxt, onValueChange = {
+            telponTxt = it
+        }, label = { Text(
+            text = "Telepon") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        Spacer(modifier = Modifier.padding(16.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(R.dimen.padding_medium))
+                .weight(1f, false),
+            horizontalArrangement =
+            Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium)),
+            verticalAlignment = Alignment.Bottom
+        ) {
+            OutlinedButton(
+                modifier = Modifier.weight(1f),
+                onClick =
+                onCancelButtonClicked) {
+                Text(stringResource(R.string.cancel))
+            }
+            Button(
+                modifier = Modifier.weight(1f),
+                enabled = namaTxt.isNotEmpty() && alamatTxt.isNotEmpty() && telponTxt.isNotEmpty(),
+                onClick = { onSubmitButtonClick(listDataTxt) }
+            ) {
+                Text(stringResource(R.string.next))
+            }
+        }
+
+    }
+
+
+}
